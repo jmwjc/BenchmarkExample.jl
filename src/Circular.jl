8e0a5eaa32a318dc7b920e99fea56f69100c9281
WuJC <jmwjc@163.com>
2026-03-19 21:14:00 +0800

    revise the circular

--- src/Circular.jl
+++ src/Circular.jl
@@ -18,13 +18,15 @@ function generateMsh(filepath::String; lc = 1.0, transfinite = -1, order = 1, qu
     gmsh.initialize()
     gmsh.model.add("Circular")
 
-    Γᵇ, Γˡ, Γᵉ, Ω = generateGeo(lc)
+    generateGeo(lc)
 
     if transfinite > 0
-        gmsh.model.mesh.setTransfiniteCurve(Γᵇ, transfinite)
-        gmsh.model.mesh.setTransfiniteCurve(Γᵉ, transfinite)
-        gmsh.model.mesh.setTransfiniteCurve(Γˡ, transfinite)
-        # gmsh.model.mesh.setTransfiniteSurface(Ω)
+        for i in 1:9
+            gmsh.model.mesh.setTransfiniteCurve(i, transfinite)
+        end
+        for i in 1:3
+            i == 2 ? gmsh.model.mesh.setTransfiniteSurface(i,"Right") : gmsh.model.mesh.setTransfiniteSurface(i)
+        end
     end
 
     if quad
@@ -48,32 +50,30 @@ end
     gmsh.model.geo.addPoint(𝑅/2^0.5, 𝑅/2^0.5, 0.0, lc, 4)
     gmsh.model.geo.addPoint(0.0, 𝑅, 0.0, lc, 5)
     gmsh.model.geo.addPoint(0.0, 𝑅/2, 0.0, lc, 6)
-    gmsh.model.geo.addPoint(𝑅/2^0.5/2, 𝑅/2^0.5/2, 0.0, lc, 7)
+    gmsh.model.geo.addPoint(2.0, 2.0, 0.0, lc, 7)
 
-    Γᵇ = gmsh.model.geo.addLine(1, 2, 1)
-    Γᵇ = gmsh.model.geo.addLine(2, 3, 2)
-    Γᵉ = gmsh.model.geo.addCircleArc(3, 1, 4, 3)
-    Γᵉ = gmsh.model.geo.addCircleArc(4, 1, 5, 4)
-    Γˡ = gmsh.model.geo.addLine(5, 6, 5)
-    Γˡ = gmsh.model.geo.addLine(6, 1, 6)
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(2, 3, 2)
+    gmsh.model.geo.addCircleArc(3, 1, 4, 3)
+    gmsh.model.geo.addCircleArc(4, 1, 5, 4)
+    gmsh.model.geo.addLine(5, 6, 5)
+    gmsh.model.geo.addLine(6, 1, 6)
     gmsh.model.geo.addLine(7, 2, 7)
     gmsh.model.geo.addLine(7, 6, 8)
     gmsh.model.geo.addLine(7, 4, 9)
     gmsh.model.geo.addCurveLoop([1,-7,8,6],1)
     gmsh.model.geo.addCurveLoop([2,3,-9,7],2)
     gmsh.model.geo.addCurveLoop([9,4,5,-8],3)
-    gmsh.model.geo.addPlaneSurface([1],1),
+    gmsh.model.geo.addPlaneSurface([1],1)
     gmsh.model.geo.addPlaneSurface([2],2)
     gmsh.model.geo.addPlaneSurface([3],3)
     gmsh.model.geo.synchronize()
 
     gmsh.model.addPhysicalGroup(0, [𝐴], -1, "𝐴")
-    gmsh.model.addPhysicalGroup(1, [Γᵇ], -1, "Γᵇ")
-    gmsh.model.addPhysicalGroup(1, [Γᵉ], -1, "Γᵉ")
-    gmsh.model.addPhysicalGroup(1, [Γˡ], -1, "Γˡ")
-    gmsh.model.addPhysicalGroup(2, [Ω], -1, "Ω")
-
-    return Γᵇ, Γᵉ, Γˡ, Ω
+    gmsh.model.addPhysicalGroup(1, [1,2], -1, "Γᵇ")
+    gmsh.model.addPhysicalGroup(1, [3,4], -1, "Γᵉ")
+    gmsh.model.addPhysicalGroup(1, [5,6], -1, "Γˡ")
+    gmsh.model.addPhysicalGroup(2, [1,2,3], -1, "Ω")
 end
 
 end
